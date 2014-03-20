5d76be12145145b2f32f2a0ce135b5fa48dce944
Michael Aldridge <aldridge.mac@gmail.com>
2014-03-20 01:21:01 -0500

    removed double buffering and bouncing balls

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -21,7 +21,7 @@
 // Last parameter = 'true' enables double-buffering, for flicker-free,
 // buttery smooth animation.  Note that NOTHING WILL SHOW ON THE DISPLAY
 // until the first call to swapBuffers().  This is normal.
-RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, true);
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 // Double-buffered mode consumes nearly all the RAM available on the
 // Arduino Uno -- only a handful of free bytes remain.  Even the
 // following string needs to go in PROGMEM:
@@ -30,16 +30,6 @@ const char str[] PROGMEM = " Disapproving Headshakes";
 int    textX   = matrix.width(),
        textMin = sizeof(str) * -12,
        hue     = 0;
-int8_t ball[3][4] = {
-  {  3,  0,  1,  1 }, // Initial X,Y pos & velocity for 3 bouncy balls
-  { 17, 15,  1, -1 },
-  { 27,  4, -1,  1 }
-};
-static const uint16_t PROGMEM ballcolor[3] = {
-  0x0080, // Green=1
-  0x0002, // Blue=1
-  0x1000  // Red=1
-};
 
 void setup() {
   matrix.begin();
@@ -49,34 +39,23 @@ void setup() {
 
 void loop() {
   byte i;
+  byte x=15;
 
   // Clear background
   matrix.fillScreen(0);
 
-  // Bounce three balls around
-  for(i=0; i<3; i++) {
-    // Draw 'ball'
-    matrix.fillCircle(ball[i][0], ball[i][1], 5, pgm_read_word(&ballcolor[i]));
-    // Update X, Y position
-    ball[i][0] += ball[i][2];
-    ball[i][1] += ball[i][3];
-    // Bounce off edges
-    if((ball[i][0] == 0) || (ball[i][0] == (matrix.width() - 1)))
-      ball[i][2] *= -1;
-    if((ball[i][1] == 0) || (ball[i][1] == (matrix.height() - 1)))
-      ball[i][3] *= -1;
-  }
-
   // Draw big scrolly text on top
   matrix.setTextColor(matrix.ColorHSV(hue, 255, 255, true));
   matrix.setCursor(textX, 1);
+  matrix.print(x);
   matrix.print(F2(str));
 
+  
   // Move text left (w/wrap), increase hue
   if((--textX) < textMin) textX = matrix.width();
   hue += 7;
   if(hue >= 1536) hue -= 1536;
-
-  // Update display
-  matrix.swapBuffers(false);
+  
+  //sleep for a few ms to smooth out the redraw
+  delay(20);
 }
